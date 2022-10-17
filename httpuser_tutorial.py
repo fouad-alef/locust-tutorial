86d26b13bd8242f1adb5ca503495faebee2ebcab
fouad-alef <fouad.shammary@alefeducation.com>
2022-10-17 17:26:03 +0400

    Http User

---
+++ httpuser_tutorial.py
@@ -0,0 +1,15 @@
+from locust import HttpUser, task, constant
+
+# HttpUser class
+## represents a HTTP user which spawns and attacks the system
+## it creates a 'client' instance of HttpSession
+## get, post, delete, patch, head, put, headers, text, status_code etc.
+
+
+class FirstLoadTest(HttpUser):
+    wait_time = constant(1)
+    host = 'http://example.com'
+
+    @task
+    def launch(self):
+        self.client.get('/')
